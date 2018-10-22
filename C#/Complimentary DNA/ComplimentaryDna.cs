2923dce5b71670b202322f57fa59b9f95f683711
Dave Brown <daveyb66@hotmail.co.uk>
2018-10-22 23:22:43 +0100

    Successful solution to Complimentary DNA

--- C#/Complimentary DNA/ComplimentaryDna.cs
+++ C#/Complimentary DNA/ComplimentaryDna.cs
@@ -1,7 +1,28 @@
+// Successful solution
+
+using System.Collections.Generic;
+
 public class DnaStrand 
     {
+        
         public static string MakeComplement(string dna)
         {
-            
+            Dictionary<char, char> complements = new Dictionary<char, char>()
+            {
+                {'A', 'T'},
+                {'C', 'G'},
+                {'G', 'C'},
+                {'T', 'A'}
+            };
+
+            char[] symbols = dna.ToCharArray();
+            char[] resultArr = new char[symbols.Length];
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                resultArr[i] = complements[symbols[i]];
+            }
+
+            return new string(resultArr);
         }
     }
