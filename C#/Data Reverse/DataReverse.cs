3871bc5aad302dc0718e2d28b9acee8256dd9398
Dave Brown <daveyb66@hotmail.co.uk>
2018-10-23 00:45:12 +0100

    Successful solution for Data Reverse

--- C#/Data Reverse/DataReverse.cs
+++ C#/Data Reverse/DataReverse.cs
@@ -1,10 +1,21 @@
 using System;
 public class Kata
 {
-  public static int[] DataReverse(int[] data)
-  {
-     
-     
-     
-  }
+    public static int[] DataReverse(int[] data)
+    {
+        int[] bits = new int[data.Length];
+        int startIndex = data.Length - 8;
+        
+        for (int i = 0; i < bits.Length; i += 8)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                bits[i + j] = data[startIndex + j];
+            }
+            startIndex -= 8;
+        }
+
+        return bits;
+    }
+
 }
