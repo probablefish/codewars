8e5417a376d9bcd785eb27e9a11ae886e4580cae
Dave Brown <daveyb66@hotmail.co.uk>
2018-10-23 14:48:30 +0100

    Solved Curing Arachnophobia in Java

--- Java/Curing Arachnophobia/Spiders.java
+++ Java/Curing Arachnophobia/Spiders.java
@@ -1,6 +1,36 @@
+// Successful solution
+
+import java.lang.*;
+
 public class Spiders
 {
+    private static String[][] legs = {{"^", "^"}, {"/\\", "/\\"}, {"/╲", "╱\\"}, {"╱╲", "╱╲"}};
+    private static String[][] bodies = {{"(", ")"}, {"((", "))"}, {"(((", ")))"}};
+
+
     public static String drawSpider(int legSize, int bodySize, char mouth, char eye) {
-        return "";
+        int TotalNumberOfEyes = (int) Math.pow(2, bodySize);
+        String spider = "";
+        
+        spider += legs[legSize - 1][0];
+        spider += bodies[bodySize - 1][0];
+
+        String eyes = getEyes(TotalNumberOfEyes / 2, eye);
+        spider += eyes;
+        spider += mouth;
+        spider += eyes;
+
+        spider += bodies[bodySize - 1][1];
+        spider += legs[legSize - 1][1];
+
+        return spider;
+    }
+
+    private static String getEyes(int numberOfEyes, char eyeType) {
+        String eyes = "";
+        for (int i = 0; i < numberOfEyes; i++){
+             eyes += eyeType;
+        }
+        return eyes;
     }
 }
