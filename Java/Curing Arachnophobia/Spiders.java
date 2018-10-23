dbb3f42b99b5ed54aa91ced17c47e402a54e3d3a
Dave Brown <daveyb66@hotmail.co.uk>
2018-10-23 14:08:57 +0100

    Started Curing Arachnophobia in Java

---
+++ Java/Curing Arachnophobia/Spiders.java
@@ -0,0 +1,6 @@
+public class Spiders
+{
+    public static String drawSpider(int legSize, int bodySize, char mouth, char eye) {
+        return "";
+    }
+}
---
+++ Java/Curing Arachnophobia/TestingSpiders.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class TestingSpiders 
+{
+    @Test
+    public void testBasic() {
+        assertEquals("^(oWo)^", Spiders.drawSpider(1, 1, 'W', 'o'));
+        
+        assertEquals("/\\((OOwOO))/\\", Spiders.drawSpider(2, 2, 'w', 'O'));
+        
+        assertEquals("/╲(((0000w0000)))╱\\", Spiders.drawSpider(3, 3, 'w', '0'));
+    }
+}
