1d4e68cd7645fbc6b03377373a3b005c24e9a2fb
Dave Brown <daveyb66@hotmail.co.uk>
2018-10-23 15:16:44 +0100

    Started 'The Lift' with Java

---
+++ Java/The Lift/TheLift.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class Dinglemouse {
+
+    public static int[] theLift(final int[][] queues, final int capacity) {
+      // Your code here
+      return new int[0];
+    }
+  
+  }
